3c9a734ed38c3ef13810e081d040cb0ae61e5216
Thomas Jones <jones762x39@gmail.com>
2017-10-24 15:39:39 -0500

    Add files via upload

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Event_Website.Startup))]
+namespace Event_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
